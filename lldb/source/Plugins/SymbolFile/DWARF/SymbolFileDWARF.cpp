370370ac752fba04b56edbbe3cb4e5af4e0c3c4e
Jan Kratochvil <jan.kratochvil@redhat.com>
2021-03-21 14:26:08 +0100

    Revert ""
    This reverts commit 0b67ea282b9fef93c802f0340f7fe4d18db846c9.

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
@@ -637,13 +637,12 @@ DWARFCompileUnit *SymbolFileDWARF::GetDWARFCompileUnit(CompileUnit *comp_unit) {
   if (!comp_unit)
     return nullptr;
 
-  // The compile unit ID is the index of the DWARF unit.
-  DWARFUnit *dwarf_cu = DebugInfo().GetUnitAtIndex(comp_unit->GetID());
-  if (dwarf_cu && dwarf_cu->GetUserData() == nullptr)
-    dwarf_cu->SetUserData(comp_unit);
+  ModuleSP module_sp = comp_unit->CalculateSymbolContextModule();
+  lldbassert(module_sp);
+  SymbolFileDWARF *dwarf =
+      llvm::cast<SymbolFileDWARF>(module_sp->GetSymbolFile());
 
-  // It must be DWARFCompileUnit when it created a CompileUnit.
-  return llvm::cast_or_null<DWARFCompileUnit>(dwarf_cu);
+  return dwarf->DebugInfo().GetDWARFCompileUnit(*comp_unit);
 }
 
 DWARFDebugRanges *SymbolFileDWARF::GetDebugRanges() {
